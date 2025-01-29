501fcfb227be716bf87d5916779a3d7b28d0558e
aaronfnp <aaronfnp@gmail.com>
2025-01-29 15:28:40 -0500

    koreanKeyMap component added
    - Created a map that has key:values pairs of all of the different letters to hangeul

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import koreanKeyMap from "./components/koreanKeyMap";
 
 function App() {
-  const [inputText, setInputText] = useState('');
-  const [outputText, setOutputText] = useState('');
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
 
   function handleChange(e) {
-    setInputText(e.target.value)
+    setInputText(e.target.value);
   }
+
+  function handleTranslate(e) {}
 
   return (
     <div className="App">
       <div>
         <h1>Korean Keyboard</h1>
         <label>Input</label>
-        <input value={inputText} onChange={handleChange}/>
+        <input value={inputText} onChange={handleChange} />
         <div>{inputText}</div>
         <label>Output</label>
-        <input />
+        <input value={outputText} />
       </div>
     </div>
   );
