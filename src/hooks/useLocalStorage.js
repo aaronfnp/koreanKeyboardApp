51a8e98d04338e55a3d7cfc062f81a10f079db52
aaronfnp <aaronfnp@gmail.com>
2025-02-05 13:21:22 -0500

    localStorage hook added
    - Added hooks folder
    - Added localStorage save

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,26 @@
+import React, { useCallback } from "react";
+import { useEffect } from "react";
+
+const useLocalStorage = (wordList, listId) => {
+  const saveWordsLocally = useCallback(() => {
+    window.localStorage.setItem("LOCAL_LISTID", JSON.stringify(listId));
+    window.localStorage.setItem("LOCAL_WORDLIST", JSON.stringify(wordList));
+  }, [wordList, listId]);
+
+  const removeLocalWordList = useCallback(() => {
+    // May want to add check to remove only if on current word list later on
+    window.localStorage.removeItem("LOCAL_LISTID", JSON.stringify(listId));
+    window.localStorage.removeItem("LOCAL_WORDLIST", JSON.stringify(wordList));
+  }, []);
+
+  useEffect(() => {
+    saveWordsLocally();
+  }, [wordList, saveWordsLocally]);
+
+  return {
+    saveWordsLocally,
+    removeLocalWordList,
+  };
+};
+
+export default useLocalStorage;
