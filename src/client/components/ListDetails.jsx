b3a062c538984adc4c5e7b25470c0bf8e91adf2f
aaronfnp <aaronfnp@gmail.com>
2025-02-11 12:18:41 -0500

    similar books mapped and styled
    - added similar book example instead of hardcoded titles
    -- Will later use other lists once that is available
    - added map function to show image and title
    - styled book list in css

--- src/client/components/ListDetails.jsx
+++ src/client/components/ListDetails.jsx
@@ -19,8 +19,19 @@ const ListDetails = ({
 `;
   const exampleCategories = `Cozy, Food, Healing, Daily-life, Countryside, Farming, Cooking`;
 
+  const exampleBooks = [
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSZO12mnqv6catSzLF8Z05r4qfDEkf2nI0eWQ&s",
+      title: "Chinese Bong Bong Club",
+    },
+    {
+      img: "https://i.namu.wiki/i/Xf940z5T_pJjOeL7oGq6pVul6bhnru1gmoN-WkpN8U1T89pY1DxXGW4lWAD_oI8_tPrzfyEcM1FZkgnPUUJ3iUFiGmZM1gAmjfrfc6csPIbrAD7KpsZHwDCh6Uo-2EEnOPQTNkH8kIFcBxK2jZw_oVOjNGTjc6FqCjZOA7wV5VI.webp",
+      title: "Omelette Rice Jam Jam",
+    },
+  ];
+
   return (
-    <main className="list-details mt-10">
+    <main className="list-details mt-10 rounded-lg">
       <h1 className="text-3xl">{storedListInfo.name}</h1>
       <h3>Description</h3>
       <p>{storedListInfo.description || "No description available"}</p>
@@ -28,11 +39,15 @@ const ListDetails = ({
       <h3>Categories</h3>
       <p>{storedListInfo.themes || "No categories"}</p>
 
-      <h3>Similar Lists</h3>
-      <ul>
-        <li>Chinese Bong Bong Club</li>
-        <li>Omelette Rice Jam Jam</li>
-      </ul>
+      <h3>Similar Books</h3>
+      <div className="similar-books-container">
+        {exampleBooks.map((b) => (
+          <div key={b.title} className="book-item">
+            <img src={b.img} alt={b.title} className="book-image" />
+            <p className="book-title">{b.title}</p>
+          </div>
+        ))}
+      </div>
 
       <StoredWords
         storedWords={storedWords}
